bd45a379b3eeeab390a0f07c2ab6fa2911003011
gamal-pegasus <gamal200224@gmail.com>
2025-04-18 22:29:34 +0200

    Updated routing and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import React, { Children } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout/Layout'
-import Invoice from './components/Invoice/Invoice'
-import DelayedShipments from './components/DelayedShipments/DelayedShipments'
-import StockList from './components/StockList/StockList'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Invoice from './components/Invoice/Invoice';
+import DelayedShipments from './components/DelayedShipments/DelayedShipments';
+import StockList from './components/StockList/StockList';
 import { Toaster } from 'react-hot-toast';
-import DailyInvoices from './components/DailyInvoices/DailyInvoices'
-import OrderTrackingCenter from './components/OrderTrackingCenter/OrderTrackingCenter'
+import DailyInvoices from './components/DailyInvoices/DailyInvoices';
+import OrderTrackingCenter from './components/OrderTrackingCenter/OrderTrackingCenter';
+
 export default function App() {
-  const routes=createBrowserRouter([
-    {path:'/',element: <Layout/>,children:[
-      {path:'/',element:<Invoice/>},
-      {path:'DelayedShipments',element:<DelayedShipments/>},
-      {path:'stocklist',element:<StockList/>},
-      {path:'DailyInvoices',element:<DailyInvoices/>},
-      {path:'OrderTrackingCenter',element:<OrderTrackingCenter/>},
-    ]}
-  ])
-  return <>
-  <Toaster position="bottom-center" reverseOrder={false}/>
-  <RouterProvider router={routes}/>
-  </>
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Invoice /> },
+        { path: 'DelayedShipments', element: <DelayedShipments /> },
+        { path: 'stocklist', element: <StockList /> },
+        { path: 'DailyInvoices', element: <DailyInvoices /> },
+        { path: 'OrderTrackingCenter', element: <OrderTrackingCenter /> },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <RouterProvider router={routes} />
+    </>
+  );
 }
